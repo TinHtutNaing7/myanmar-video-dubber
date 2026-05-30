c446eda4ef1cce0fa7be1fed6998ff4ff99af171
TinHtutNaing7 <83341325+TinHtutNaing7@users.noreply.github.com>
2026-05-31 06:58:38 +0900

    Add files via upload

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware(_request: NextRequest) {
   const response = NextResponse.next();
   response.headers.set("Cross-Origin-Embedder-Policy", "require-corp");
   response.headers.set("Cross-Origin-Opener-Policy",   "same-origin");
